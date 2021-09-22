3be31af1fb624811b889f30f252e114ede2c7a11
krishnaveni610 <krishnaveni610@gmail.com>
2021-09-22 19:53:09 +0530

    Add files via upload

---
+++ lc-new1/src/main/java/org/lc/lcnew1/LcNew1Application.java
@@ -0,0 +1,13 @@
+package org.lc.lcnew1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcNew1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LcNew1Application.class, args);
+	}
+
+}
---
+++ lc-new1/src/test/java/org/lc/lcnew1/LcNew1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lc.lcnew1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcNew1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
